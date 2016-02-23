6b38b407d1f39757a9ac1cdcedcfac037dcb6ee5
romankormylo <kormyloroman@yahoo.co.uk>
2016-02-23 14:59:17 +0000

    only js

---
+++ equation.js
@@ -0,0 +1,11 @@
+function equation(a,b,c) {
+    var equation1,equation2;
+    var equaTop = Math.sqrt(b*b-4*a*c);
+    var equaBottom = 2*a;
+    var x1 = (-b + equaTop)/equaBottom;
+    var x2 = (-b - equaTop)/equaBottom;
+    equation1=a*(x1*x1)+b*x1+c;
+    equation2 = a*(x2*x2)+b*x2+c;
+    return('(x1) ' + x1 + ' = ' + equation1 + '    ' + '(x2) ' +x2 + ' = ' + equation2);
+}
+console.log(equation(-2,4,4));
